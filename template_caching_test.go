27c7a2da19e4a6fce1898299572cbe2c6b15448b
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-19 00:01:56 +0700

    Template Caching

---
+++ template_caching_test.go
@@ -0,0 +1,30 @@
+package go_web
+
+import (
+	"embed"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"text/template"
+)
+
+//go:embed templates/*.gohtml
+var templates embed.FS
+
+var myTemplates = template.Must(template.ParseFS(templates, "templates/*.gohtml")) //best practice
+
+func TemplateCaching(w http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(w, "simple.gohtml", "Hello Template Caching")
+}
+
+func TestTemplateCaching(t *testing.T) {
+	requst := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateCaching(recorder, requst)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}

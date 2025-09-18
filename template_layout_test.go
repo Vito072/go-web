fd73f659a687f3133cf7451c2ad9aaf90d7b42af
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-18 23:04:33 +0700

    Template Layout

---
+++ template_layout_test.go
@@ -0,0 +1,32 @@
+package go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateLayout(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"./templates/header.gohtml",
+		"./templates/layout.gohtml",
+		"./templates/footer.gohtml",
+	))
+	t.ExecuteTemplate(w, "layout.gohtml", map[string]interface{}{
+		"Title": "Template Layout",
+		"Name":  "Vito",
+	})
+}
+
+func TestTemplateLayout(t *testing.T) {
+	requst := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, requst)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}

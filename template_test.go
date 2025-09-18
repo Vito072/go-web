2bed4174ea3d8c086383397c535e08a14964d041
Vito Jihad Abdillah <jihadvito@gmail.com>
2025-09-18 21:59:12 +0700

    Template HTML Go

---
+++ template_test.go
@@ -0,0 +1,47 @@
+package go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SimpleHTML(w http.ResponseWriter, r *http.Request) {
+	templateText := "<html><body>{{.}}</body></html>"
+	// t, err := template.New("SIMPLE").Parse(templateText)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	t := template.Must(template.New("SIMPLE").Parse(templateText))
+
+	t.ExecuteTemplate(w, "SIMPLE", "Hello HTML Template")
+}
+
+func TestSimpleHTML(t *testing.T) {
+	requst := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTML(recorder, requst)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func SimpleHTMLFile(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/simple.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Hello HTML")
+}
+
+func TestSimpleHTMLFile(t *testing.T) {
+	requst := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHTMLFile(recorder, requst)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
